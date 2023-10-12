0df99201975a2dfe0f9147e69ec1634396f4c2f3
jrheard <jrheard@cs.stanford.edu>
2023-10-12 14:30:47 -0700

    day 5 part 2

--- day_5.py
+++ day_5.py
@@ -27,19 +27,26 @@ def parse_boarding_pass(boarding_pass: str) -> tuple[int, int]:
     assert row_range[0] == row_range[1]
     assert col_range[0] == col_range[1]
 
-    return row_range[0], col_range[0]
+    # Every seat also has a unique seat ID: multiply the row by 8, then add the column.
+    return row_range[0] * 8 + col_range[0]
 
 
 def part_1() -> int:
-    # Every seat also has a unique seat ID: multiply the row by 8, then add the column.
     # As a sanity check, look through your list of boarding passes. What is the highest seat ID on a boarding pass?
-    return max(
-        row * 8 + col for row, col in map(parse_boarding_pass, load_boarding_passes())
-    )
+    return max(map(parse_boarding_pass, load_boarding_passes()))
 
 
 def part_2() -> int:
-    pass
+    # It's a completely full flight, so your seat should be the only missing
+    # boarding pass in your list. However, there's a catch: some of the seats at
+    # the very front and back of the plane don't exist on this aircraft, so
+    # they'll be missing from your list as well.  Your seat wasn't at the very
+    # front or back, though; the seats with IDs +1 and -1 from yours will be in
+    # your list.  What is the ID of your seat?
+    seats = list(sorted(map(parse_boarding_pass, load_boarding_passes())))
+    for i, seat in enumerate(seats):
+        if seats[i + 1] - seat > 1:
+            return seat + 1
 
 
 if __name__ == "__main__":
