f6f6732640867f316158feb088e854c487bfb62c
jrheard <jrheard@cs.stanford.edu>
2023-10-12 13:58:10 -0700

    day 4 part 2

--- day_4.py
+++ day_4.py
@@ -1,4 +1,28 @@
-ALL_PASSPORT_FIELDS = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"}
+import re
+
+
+def height_is_valid(passport_value: str) -> bool:
+    if passport_value.endswith("cm"):
+        return 150 <= int(passport_value[:-2]) <= 193
+    elif passport_value.endswith("in"):
+        return 59 <= int(passport_value[:-2]) <= 76
+    else:
+        return False
+
+
+PASSPORT_FIELD_VALIDATORS = {
+    "byr": lambda v: 1920 <= int(v) <= 2002,
+    "iyr": lambda v: 2010 <= int(v) <= 2020,
+    "eyr": lambda v: 2020 <= int(v) <= 2030,
+    "hgt": height_is_valid,
+    "hcl": lambda v: re.match(r"#[0-9a-f]{6,6}", v),
+    "ecl": lambda v: v in {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"},
+    "pid": lambda v: re.match(r"^\d{9,9}$", v),
+    "cid": lambda v: True,
+}
+
+
+ALL_PASSPORT_FIELDS = PASSPORT_FIELD_VALIDATORS.keys()
 
 
 def parse_input() -> list[dict[str:str]]:
@@ -30,19 +54,28 @@ def parse_input() -> list[dict[str:str]]:
     return passports
 
 
-def is_passport_valid(passport: dict[str, str]) -> bool:
+def does_passport_have_required_keys(passport: dict[str, str]) -> bool:
     # it's ok to be missing 'cid', but all other fields must be present
     missing_keys = ALL_PASSPORT_FIELDS - set(passport.keys())
     return not missing_keys or missing_keys == {"cid"}
 
 
+def is_passport_valid(passport: dict[str, str]) -> bool:
+    return does_passport_have_required_keys(passport) and all(
+        PASSPORT_FIELD_VALIDATORS[k](v) for (k, v) in passport.items()
+    )
+
+
 def part_1() -> int:
     passports = parse_input()
-    return sum(1 for passport in passports if is_passport_valid(passport))
+    return sum(
+        1 for passport in passports if does_passport_have_required_keys(passport)
+    )
 
 
 def part_2() -> int:
-    pass
+    passports = parse_input()
+    return sum(1 for passport in passports if is_passport_valid(passport))
 
 
 if __name__ == "__main__":
