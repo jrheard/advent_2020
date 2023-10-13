59e8bf69668eca796fa5c1f1692410d620d17acf
jrheard <jrheard@cs.stanford.edu>
2023-10-13 12:55:25 -0700

    WIP

--- day_8.py
+++ day_8.py
@@ -1,19 +1,24 @@
-def load_input() -> list[tuple[str, int]]:
+Instruction = tuple[str, int]
+
+
+def load_input() -> list[Instruction]:
     with open("inputs/day_8.txt") as f:
         lines = [line.strip().split(" ") for line in f]
 
     return [(instruction, int(value)) for instruction, value in lines]
 
 
-def part_1() -> int:
-    instructions = load_input()
-
+def does_program_contain_an_infinite_loop(
+    instructions: list[Instruction],
+) -> tuple[bool, int]:
     instruction_pointer = 0
     visited_instructions = set()
     accumulator = 0
 
-    # Immediately before any instruction is executed a second time, what value is in the accumulator?
     while True:
+        if instruction_pointer == len(instructions):
+            return False, accumulator
+
         match instructions[instruction_pointer]:
             case ["acc", num]:
                 accumulator += num
@@ -21,14 +26,17 @@ def part_1() -> int:
             case ["jmp", num]:
                 instruction_pointer += num
                 if instruction_pointer in visited_instructions:
-                    # We've visited this instruction before - time to return the accumulator!
-                    break
+                    # We've visited this instruction before - the program contains an infinite loop!
+                    return True, accumulator
             case ["nop", _]:
                 instruction_pointer += 1
 
         visited_instructions.add(instruction_pointer)
 
-    return accumulator
+
+def part_1() -> int:
+    # Immediately before any instruction is executed a second time, what value is in the accumulator?
+    return does_program_contain_an_infinite_loop(load_input())[1]
 
 
 def part_2() -> int:
