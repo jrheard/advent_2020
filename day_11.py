8c782d5429f208e9d921d2e617389c9e8032469c
jrheard <jrheard@cs.stanford.edu>
2023-10-16 14:10:39 -0700

    day 11 part 2

--- day_11.py
+++ day_11.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from enum import Enum
+from enum import Enum, auto
 from dataclasses import dataclass
 from functools import lru_cache
 import itertools
@@ -15,6 +15,11 @@ class GridValue(Enum):
     EMPTY_SEAT = "L"
     OCCUPIED_SEAT = "#"
     FLOOR = "."
+
+
+class WhichNeighborAlgorithmToUse(Enum):
+    IMMEDIATELY_ADJACENT = auto()
+    LINE_OF_SIGHT = auto()
 
 
 @dataclass(frozen=True)
@@ -39,7 +44,54 @@ class SeatGrid:
             if self.value_at(xx, yy) == GridValue.OCCUPIED_SEAT
         )
 
-    def one_round_later(self) -> SeatGrid:
+    def value_of_first_visible_seat_in_direction(
+        self, x: int, y: int, direction: tuple[int, int]
+    ) -> GridValue:
+        x_delta, y_delta = direction
+
+        while True:
+            if not (0 <= x + x_delta < self.width) or not (
+                0 <= y + y_delta < self.height
+            ):
+                # Didn't find any seats in this direction, occupied or otherwise.
+                return GridValue.FLOOR
+
+            value = self.value_at(x + x_delta, y + y_delta)
+            if value != GridValue.FLOOR:
+                # Found a seat!
+                return value
+
+            x_delta += direction[0]
+            y_delta += direction[1]
+
+    def num_occupied_neighbors_by_line_of_sight(self, x: int, y: int) -> int:
+        # People don't just care about adjacent seats - they care about the
+        # first seat they can see in each of those eight directions! Now,
+        # instead of considering just the eight immediately adjacent seats,
+        # consider the first seat in each of those eight directions.
+        directions = [
+            (-1, -1),
+            (0, -1),
+            (1, -1),
+            (-1, 0),
+            (1, 0),
+            (-1, 1),
+            (0, 1),
+            (1, 1),
+        ]
+
+        return sum(
+            1
+            for direction in directions
+            if self.value_of_first_visible_seat_in_direction(x, y, direction)
+            == GridValue.OCCUPIED_SEAT
+        )
+
+    def one_round_later(
+        self,
+        min_occupied_seats_for_occupied_seat_to_become_empty: int,
+        neighbor_algorithm: WhichNeighborAlgorithmToUse,
+    ) -> SeatGrid:
         # The following rules are applied to every seat simultaneously:
         new_seat_values = []
 
@@ -47,15 +99,20 @@ class SeatGrid:
             for x in range(0, self.width):
                 value = self.value_at(x, y)
 
-                if (
-                    value == GridValue.EMPTY_SEAT
-                    and self.num_occupied_neighbors(x, y) == 0
-                ):
+                neighbor_method = (
+                    self.num_occupied_neighbors
+                    if neighbor_algorithm
+                    == WhichNeighborAlgorithmToUse.IMMEDIATELY_ADJACENT
+                    else self.num_occupied_neighbors_by_line_of_sight
+                )
+
+                if value == GridValue.EMPTY_SEAT and neighbor_method(x, y) == 0:
                     # If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
                     new_value = GridValue.OCCUPIED_SEAT
                 elif (
                     value == GridValue.OCCUPIED_SEAT
-                    and self.num_occupied_neighbors(x, y) >= 4
+                    and neighbor_method(x, y)
+                    >= min_occupied_seats_for_occupied_seat_to_become_empty
                 ):
                     # If a seat is occupied (#) and four or more seats adjacent to it are also occupied, the seat becomes empty.
                     new_value = GridValue.EMPTY_SEAT
@@ -96,7 +153,9 @@ def part_1() -> int:
     # Simulate your seating area by applying the seating rules repeatedly until no seats change state.
     # How many seats end up occupied?
     while True:
-        next_grid = grid.one_round_later()
+        next_grid = grid.one_round_later(
+            4, WhichNeighborAlgorithmToUse.IMMEDIATELY_ADJACENT
+        )
 
         if grid == next_grid:
             break
@@ -107,7 +166,21 @@ def part_1() -> int:
 
 
 def part_2() -> int:
-    return -1
+    grid = load_input()
+
+    # Given the new visibility method and the rule change for occupied seats
+    # becoming empty, once equilibrium is reached, how many seats end up occupied?
+    while True:
+        next_grid = grid.one_round_later(
+            5, WhichNeighborAlgorithmToUse.LINE_OF_SIGHT
+        )
+
+        if grid == next_grid:
+            break
+
+        grid = next_grid
+
+    return sum(1 for seat in grid.seats if seat == GridValue.OCCUPIED_SEAT)
 
 
 if __name__ == "__main__":
