656bbffe0db1a597ea68ade7f83fb05abe464a85
jrheard <jrheard@cs.stanford.edu>
2023-10-16 15:35:51 -0700

    day 12 part 2

--- day_12.py
+++ day_12.py
@@ -22,28 +22,36 @@ DELTAS_BY_DIRECTION = {
 }
 
 
+def compute_new_direction_relative_to_old_one(
+    old_direction: Direction, relative_direction: Literal["L", "R"], degrees: int
+) -> Direction:
+    current_direction_index = DIRECTIONS_IN_ORDER.index(old_direction)
+
+    value_in_indexes = degrees / 90
+
+    multiplier = -1 if relative_direction == "L" else 1
+
+    new_index = current_direction_index + int(value_in_indexes * multiplier)
+
+    # We don't want to use modulus here because it doesn't handle negative numbers the way we want (eg -1 % 4 == 3, not -1)
+    if new_index >= 4:
+        new_index -= 4
+
+    return DIRECTIONS_IN_ORDER[new_index]
+
+
 @dataclass(frozen=True)
 class Ship:
     facing_direction: Direction
     position: tuple[int, int]
 
     def turn_in_relative_direction(
-        self, relative_direction: Literal["L", "R"], value: int
+        self, relative_direction: Literal["L", "R"], degrees: int
     ) -> Ship:
-        current_direction_index = DIRECTIONS_IN_ORDER.index(self.facing_direction)
-
-        value_in_indexes = value / 90
-
-        multiplier = -1 if relative_direction == "L" else 1
-
-        new_index = current_direction_index + int(value_in_indexes * multiplier)
-
-        # We don't want to use modulus here because it doesn't handle negative numbers the way we want (eg -1 % 4 == 3, not -1)
-        if new_index >= 4:
-            new_index -= 4
-
         return Ship(
-            facing_direction=DIRECTIONS_IN_ORDER[new_index],
+            facing_direction=compute_new_direction_relative_to_old_one(
+                self.facing_direction, relative_direction, degrees
+            ),
             position=self.position,
         )
 
@@ -86,8 +94,76 @@ def part_1() -> int:
     return abs(ship.position[0]) + abs(ship.position[1])
 
 
+def rotate_waypoint_right(waypoint_x: int, waypoint_y: int) -> tuple[int, int]:
+    return (-waypoint_y, waypoint_x)
+
+
+def rotate_waypoint_left(waypoint_x: int, waypoint_y: int) -> tuple[int, int]:
+    return (waypoint_y, -waypoint_x)
+
+
+@dataclass(frozen=True)
+class WaypointedShip:
+    position: tuple[int, int]
+    waypoint: tuple[int, int]
+
+    def move_waypoint_in_direction(
+        self, direction: Direction, value: int
+    ) -> WaypointedShip:
+        x_delta, y_delta = DELTAS_BY_DIRECTION[direction]
+        return WaypointedShip(
+            position=self.position,
+            waypoint=(
+                self.waypoint[0] + x_delta * value,
+                self.waypoint[1] + y_delta * value,
+            ),
+        )
+
+    def move_waypoint_in_relative_direction(
+        self, relative_direction: Literal["L", "R"], degrees: int
+    ) -> WaypointedShip:
+        move_fn = (
+            rotate_waypoint_left if relative_direction == "L" else rotate_waypoint_right
+        )
+        num_times = degrees // 90
+        new_waypoint = self.waypoint
+        for _ in range(num_times):
+            new_waypoint = move_fn(*new_waypoint)
+
+        return WaypointedShip(position=self.position, waypoint=new_waypoint)
+
+    def move_to_waypoint(self, num_times: int) -> WaypointedShip:
+        new_x, new_y = self.position
+        for _ in range(num_times):
+            new_x += self.waypoint[0]
+            new_y += self.waypoint[1]
+
+        return WaypointedShip(position=(new_x, new_y), waypoint=self.waypoint)
+
+    def process_action(self, action: tuple[str, int]) -> WaypointedShip:
+        match action:
+            case "L" | "R", action_value:
+                return self.move_waypoint_in_relative_direction(*action)
+            case "F", action_value:
+                return self.move_to_waypoint(action_value)
+            case direction_str, action_value:
+                return self.move_waypoint_in_direction(
+                    Direction(direction_str), action_value
+                )
+            case _:
+                raise ValueError(f"Invalid action {action}")
+
+
 def part_2() -> int:
-    return -1
+    # The waypoint starts 10 units east and 1 unit north relative to the ship.
+    ship = WaypointedShip((0, 0), (10, -1))
+
+    for action in load_input():
+        ship = ship.process_action(action)
+
+    # Figure out where the navigation instructions lead. What is the Manhattan
+    # distance between that location and the ship's starting position?
+    return abs(ship.position[0]) + abs(ship.position[1])
 
 
 if __name__ == "__main__":
