724ca81810d0d5f9c24f0731b409f9ee9b5a4a89
jrheard <jrheard@cs.stanford.edu>
2023-10-17 15:10:59 -0700

    day 16 part 2

--- day_16.py
+++ day_16.py
@@ -1,3 +1,5 @@
+import functools
+
 # The type of a dict representing input lines like:
 #     departure location: 32-174 or 190-967
 # with data like this:
@@ -49,7 +51,7 @@ def find_invalid_value_in_ticket(ticket: tuple[int], field_ranges: FieldRanges) 
 
 
 def part_1() -> int:
-    field_ranges, your_ticket, nearby_tickets = parse_input()
+    field_ranges, _, nearby_tickets = parse_input()
 
     return sum(
         find_invalid_value_in_ticket(ticket, field_ranges) for ticket in nearby_tickets
@@ -57,7 +59,71 @@ def part_1() -> int:
 
 
 def part_2() -> int:
-    return -1
+    field_ranges, your_ticket, nearby_tickets = parse_input()
+
+    valid_tickets = [
+        ticket
+        for ticket in nearby_tickets
+        if find_invalid_value_in_ticket(ticket, field_ranges) == 0
+    ]
+
+    # `your_ticket` has N ordered field values - right now, any one of those values
+    # could correspond to any field (eg 'departure location', 'arrival platform', etc).
+    field_candidates_by_index = [
+        set(field_ranges.keys()) for _ in range(len(valid_tickets[0]))
+    ]
+
+    # First, we eliminate all of the fields that _definitely_ can't possibly live at a particular index
+    # by seeing which indexes have invalid values for each field.
+    # For instance, if we have this rule:
+    #   departure location: 32-174 or 190-967
+    # and if a ticket has the value 180 at index 0,
+    # then we know that index 0 can't be for the field "departure location".
+    for ticket in valid_tickets:
+        for i, field_value in enumerate(ticket):
+            for candidate_field in field_candidates_by_index[i].copy():
+                range_1, range_2 = field_ranges[candidate_field]
+                if not (
+                    range_1[0] <= field_value <= range_1[1]
+                    or range_2[0] <= field_value <= range_2[1]
+                ):
+                    field_candidates_by_index[i].remove(candidate_field)
+
+    # At this point, the way that our inputs are structured, we'll be left with one index
+    # that has a clear answer (i.e. has exactly one candidate field), and another index with
+    # two possible fields, and another index with three fields, etc.
+    #
+    # All we need to do now is use process of elimination - e.g. if the field 'row' has been proven
+    # to live at index 2, it definitely cannot live at any other index.
+
+    field_indexes_by_field_name = {}
+
+    while any(len(candidates) > 0 for candidates in field_candidates_by_index):
+        index, converged_field = next(
+            (i, candidates.pop())
+            for i, candidates in enumerate(field_candidates_by_index)
+            if len(candidates) == 1
+        )
+        field_indexes_by_field_name[converged_field] = index
+
+        for candidates in field_candidates_by_index:
+            if len(candidates) > 0:
+                candidates.remove(converged_field)
+
+    # Done!
+    assert len(field_indexes_by_field_name) == len(your_ticket)
+
+    # "Once you work out which field is which, look for the six fields on your
+    # ticket that start with the word departure. What do you get if you multiply
+    # those six values together?"
+    return functools.reduce(
+        lambda x, y: x * y,
+        (
+            your_ticket[index]
+            for field_name, index in field_indexes_by_field_name.items()
+            if field_name.startswith("departure")
+        ),
+    )
 
 
 if __name__ == "__main__":
