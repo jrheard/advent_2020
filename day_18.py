d5a6b9af01e0408a542a29edc9232fe003465be8
jrheard <jrheard@cs.stanford.edu>
2023-10-18 14:52:27 -0700

    WIP

--- day_18.py
+++ day_18.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from typing import Literal
 
 
 @dataclass
 class Expression:
     left: int | Expression
-    right: Expression | None
+    right: int | Expression | None
     operator: str | None
     parentheses_depth: int
 
@@ -56,9 +57,39 @@ def evaluate_leaf_expression(expression: Expression) -> int:
         if expression.right is None:
             break
 
+        assert isinstance(expression.right, Expression)
         expression = expression.right
 
     return result
+
+
+def find_parent_of_deepest_parenthesized_expression(
+    expression,
+) -> tuple[Expression, Literal["left", "right"]] | None:
+    parent = expression
+    max_depth = 0
+    # TODO
+
+    pass
+
+
+def replace_parenthesized_expressions_with_ints(expression: Expression) -> None:
+    parent_info = find_parent_of_deepest_parenthesized_expression(expression)
+
+    if not parent_info:
+        # If there are no nested parenthesized expressions, we're done!
+        return
+
+    parent, direction = parent_info
+    if direction == "left":
+        assert isinstance(parent.left, Expression)
+        parent.left = evaluate_leaf_expression(parent.left)
+    else:
+        assert isinstance(parent.right, Expression)
+        parent.right = evaluate_leaf_expression(parent.right)
+
+    # Recur to replace the next-deepest parenthesized expression.
+    replace_parenthesized_expressions_with_ints(expression)
 
 
 def load_input() -> list[str]:
@@ -83,54 +114,6 @@ def find_position_of_matching_closing_parenthesis(string: str) -> int:
                 num_open_parentheses_seen -= 1
 
     return -1
-
-
-def evaluate_parenthesized_expression(expression: str) -> int:
-    closing_paren_index = find_position_of_matching_closing_parenthesis(expression)
-    leftmost_value = evaluate_expression(expression[1:closing_paren_index])
-
-    if closing_paren_index == len(expression) - 1:
-        return leftmost_value
-
-    operator = expression[closing_paren_index + 2]
-    rest_of_expression = expression[closing_paren_index + 4 :]
-
-
-def evaluate_expression(expression: str) -> int:
-    # "However, the rules of operator precedence have changed. Rather than
-    # evaluating multiplication before addition, the operators have the same
-    # precedence, and are evaluated left-to-right regardless of the order in
-    # which they appear."
-
-    if expression[0] == "(":
-        closing_paren_index = find_position_of_matching_closing_parenthesis(expression)
-        leftmost_value = evaluate_expression(expression[1:closing_paren_index])
-
-        if closing_paren_index == len(expression) - 1:
-            return leftmost_value
-
-        operator = expression[closing_paren_index + 2]
-        rest_of_expression = expression[closing_paren_index + 4 :]
-
-    else:
-        leftmost_value = int(expression[0])
-
-        if len(expression) == 1:
-            return leftmost_value
-
-        operator = expression[2]
-        rest_of_expression = expression[4:]
-
-    if operator == "+":
-        print(
-            f"{expression=}, {rest_of_expression=}, {leftmost_value=} + {evaluate_expression(rest_of_expression)=}"
-        )
-        return leftmost_value + evaluate_expression(rest_of_expression)
-    else:
-        print(
-            f"{expression=}, {rest_of_expression=}, {leftmost_value=} * {evaluate_expression(rest_of_expression)=}"
-        )
-        return leftmost_value * evaluate_expression(rest_of_expression)
 
 
 def part_1() -> int:
