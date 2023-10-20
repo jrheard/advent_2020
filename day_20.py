219690a0b53fbbe7d04774b59a048128cc0141ca
jrheard <jrheard@cs.stanford.edu>
2023-10-20 15:37:43 -0700

    WIP

--- day_20.py
+++ day_20.py
@@ -26,7 +26,7 @@ def print_tile_data(tile: Tile) -> None:
 
 
 def rotate_tile_right(tile: Tile) -> Tile:
-    print(f"rotating {tile.id} to the right")
+    # print(f"rotating {tile.id} to the right")
     rotated_data = ["" for _ in tile.data]
 
     for line in tile.data:
@@ -92,64 +92,68 @@ def place_tiles(tiles: list[Tile]) -> dict[tuple[int, int], Tile]:
     unplaced_tiles = [tile for tile in tiles if tile != first_corner]
 
     while unplaced_tiles:
+        print(len(placed_tiles), len(unplaced_tiles))
         matches = find_matches(list(placed_tiles.values()) + unplaced_tiles)
 
-        for tile in unplaced_tiles:
-            try:
-                relevant_match = next(
+        tile, relevant_matches = next(
+            (tile, relevant_matches)
+            for tile in unplaced_tiles
+            if (
+                relevant_matches := [
                     match
                     for match in matches
                     if match.tile_2_id == tile.id
                     and match.tile_1_id in {tile.id for tile in placed_tiles.values()}
-                )
-            except StopIteration:
-                continue
-
-            # TODO infinite loop
-
-            # Remember, a match looks like this:
-            #   Match(tile_1_id=1117, tile_1_border_index=3, tile_2_id=2003, tile_2_border_index=2)
-
-            # relevant_match.tile_1_id is a placed tile whose position and orientation are fully known.
-            # relevant_match.tile_2_id is `tile`, whose position is unknown and whose orientation is arbitrary.
-
-            # First, let's figure out `tile`'s correct orientation.
-            # If the match occurs along tile 1's east border, then it occurs along tile 2's west border;
-            # if it occurs along tile 1's south border, then it occurs along tile 2's north border; etc.
-
-            correctly_rotated_tile_2_border_index = (
-                relevant_match.tile_1_border_index + 2
-            ) % 4
-
-            # If correctly_rotated_tile_2_border_index == relevant_match.tile_2_border_index, then we're all set.
-            # Otherwise, we need to rotate tile 2 to the right 1-3 times.
-            num_times_rotated_right = 0
-            while (
-                relevant_match.tile_2_border_index + num_times_rotated_right
-            ) % 4 != correctly_rotated_tile_2_border_index:
-                tile = rotate_tile_right(tile)
-                num_times_rotated_right += 1
-
-            # Now all we have to do is figure out this tile's placement position.
-            placed_tile_x, placed_tile_y = next(
-                position
-                for position, placed_tile in placed_tiles.items()
-                if placed_tile.id == relevant_match.tile_1_id
+                ]
             )
-            if relevant_match.tile_1_border_index == 0:
-                position = (placed_tile_x, placed_tile_y - 1)
-            elif relevant_match.tile_1_border_index == 1:
-                position = (placed_tile_x + 1, placed_tile_y)
-            elif relevant_match.tile_1_border_index == 2:
-                position = (placed_tile_x, placed_tile_y + 1)
-            else:
-                position = (placed_tile_x - 1, placed_tile_y)
+        )
 
-            placed_tiles[position] = tile
+        relevant_match = relevant_matches[0]
 
-            unplaced_tiles = [
-                other_tile for other_tile in unplaced_tiles if other_tile != tile
-            ]
+        # Remember, a match looks like this:
+        #   Match(tile_1_id=1117, tile_1_border_index=3, tile_2_id=2003, tile_2_border_index=2)
+
+        # relevant_match.tile_1_id is a placed tile whose position and orientation are fully known.
+        # relevant_match.tile_2_id is `tile`, whose position is unknown and whose orientation is arbitrary.
+
+        # First, let's figure out `tile`'s correct orientation.
+        # If the match occurs along tile 1's east border, then it occurs along tile 2's west border;
+        # if it occurs along tile 1's south border, then it occurs along tile 2's north border; etc.
+
+        correctly_rotated_tile_2_border_index = (
+            relevant_match.tile_1_border_index + 2
+        ) % 4
+
+        # If correctly_rotated_tile_2_border_index == relevant_match.tile_2_border_index, then we're all set.
+        # Otherwise, we need to rotate tile 2 to the right 1-3 times.
+        num_times_rotated_right = 0
+        while (
+            relevant_match.tile_2_border_index + num_times_rotated_right
+        ) % 4 != correctly_rotated_tile_2_border_index:
+            tile = rotate_tile_right(tile)
+            num_times_rotated_right += 1
+
+        # Now all we have to do is figure out this tile's placement position.
+        placed_tile_x, placed_tile_y = next(
+            position
+            for position, placed_tile in placed_tiles.items()
+            if placed_tile.id == relevant_match.tile_1_id
+        )
+        if relevant_match.tile_1_border_index == 0:
+            position = (placed_tile_x, placed_tile_y - 1)
+        elif relevant_match.tile_1_border_index == 1:
+            position = (placed_tile_x + 1, placed_tile_y)
+        elif relevant_match.tile_1_border_index == 2:
+            position = (placed_tile_x, placed_tile_y + 1)
+        else:
+            position = (placed_tile_x - 1, placed_tile_y)
+
+        assert position not in placed_tiles
+        placed_tiles[position] = tile
+
+        unplaced_tiles = [
+            other_tile for other_tile in unplaced_tiles if other_tile != tile
+        ]
 
     return placed_tiles
 
